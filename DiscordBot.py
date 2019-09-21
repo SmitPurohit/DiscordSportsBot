b399752396935749ec7a7da9b53b1dc11101c01e
SmitPurohit <35670650+SmitPurohit@users.noreply.github.com>
2019-09-21 01:15:31 -0500

    Update DiscordBot.py

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,9 +1,7 @@
-#TODO: Read xml file from http://www.nfl.com/liveupdate/scorestrip/ss.xml
-#Update scores as they come in
-#Version 1 should focus on just getting the score to send to the discord
-#Version 2 should send an update every time the score changes
-#Version 2.5 should allow users to request the score of a currently going on game
-#Version 3 should allow users to "subscribe" to a team
+#Reads from an xml site and uses Discord API to create a bot that gives the 
+#next upcoming game/score of a finished game of a requested team
+#Currently Working on making code look better
+#Next up is if !game is called when a game is going on
 import bs4 as bs
 import urllib.request
 import discord
@@ -20,52 +18,71 @@ source = urllib.request.urlopen(url).read()
 soup = bs.BeautifulSoup(source, 'lxml')
 
 root = soup.find_all('g')
-game = ""
 location = ""
 opponent = ""
-
-for tag in root:
-    homeDict = tag.attrs['hnn']
-    vDict = tag.attrs['vnn']
-    if(homeDict == 'bears' or vDict == 'bears'):
-        if(homeDict == 'bears'):
-            location = " at home"
-            opponent = vDict
-        else:
-            location = " away"
-            opponent = homeDict
-        time = str((int)(tag.attrs['t'][0])-1)+":"+str((int)(tag.attrs['t'][2:4]))
-        day = tag.attrs['d']
-        if(day == 'Thu'):
-            day = day + 'rsday'
-        else:
-            day = day + 'day'
-        game = "The Bears play the " + opponent.capitalize() + location +  " on " + day + " at " + time
-        
-   
-
+def getGameStats(team):
+    for tag in root:
+        if(homeDict == team or vDict == team):
+            gameStatus = tag.attrs['q']
+            homeDict = tag.attrs['hnn']
+            vDict = tag.attrs['vnn']
+            vScore = tag.attrs['vs']
+            homeScore = tag.attrs['hs']            
+            if(homeDict == team):
+                location = " at home"
+                opponent = vDict
+                teamScore = (int)(homeScore)
+                oppScore = (int)(vScore)
+            else:
+                location = " away"
+                opponent = homeDict
+                teamScore = (int)(vScore)
+                oppScore = (int)(homeScore)
+            if(gameStatus == 'P'):
+                    hour = str((int)(tag.attrs['t'][0])-1)
+                    if(hour == "0"):
+                        hour = "12"
+                    minute = str((int)(tag.attrs['t'][2:4]))
+                    if(len(minute)==1):
+                        minute += "0"
+                    time = hour+":"+minute
+                    day = tag.attrs['d']
+                    if(day == 'Thu'):
+                        day = day + 'rsday'
+                    else:
+                        day = day + 'day'
+                    return "The " + team.capitalize() + " play the " + opponent.capitalize() + location +  " on " + day + " at " + time
+            if(gameStatus == 'F'):
+                    win = "tied"
+                    if(teamScore > oppScore):
+                        win = " won against "
+                    if(teamScore < oppScore):
+                        win = " lost to "
+                    return "The " + team.capitalize() + win + "the " + opponent.capitalize() + ", " + str(teamScore) + "-" + str(oppScore)
+            else:
 @client.event
 async def on_message(message):
     #We do not want the bot to reply to itself
     if message.author == client.user:
         return
     if message.content.startswith('!game'):
+
         channel = message.channel
-        
-        await message.channel.send(game)
+
+        await message.channel.send(getGameStats(message.content[6:]))
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
-        
+
 
 @client.event
 async def on_ready():
-    
+
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
     print('------')
     #await client.get_channel(619719025219600428).send(game)
-    
+
 
 client.run(TOKEN)
